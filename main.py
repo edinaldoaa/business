b3c8fdbda45d9d3e2f1227f41a88ded0395ace4d
edinaldoaa <101063390+edinaldoaa@users.noreply.github.com>
2022-08-14 22:51:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/cad/usuario")
+def usuario():
+    return render_template('usuario.html', titulo="Cadastro de Usuario")
+
+@app.route("/cad/caduser", methods=['POST'])
+def caduser():
+    return request.form
+
+@app.route("/cad/anuncio")
+def anuncio():
+    return render_template('anuncio.html')
+
+@app.route("/anuncios/pergunta")
+def pergunta():
+    return render_template('pergunta.html')
+
+
+@app.route("/anuncios/favorito")
+def favorito():
+    return render_template('favorito.html')
+
+
+@app.route("/anuncios/compra")
+def compra():
+    print("anuncio comprado")
+    return ""
+
+@app.route("/anuncio/favoritos")
+def favoritos():
+    print("favorito inserido")
+    return f"<h4>Comprado</h4>"
+
+@app.route("/config/categoria")
+def categoria():
+    return render_template('categoria.html')
+
+@app.route("/relatorios/vendas")
+def relVendas():
+    return render_template('relVendas.html')
+
+@app.route("/relatorios/compras")
+def relCompras():
+    return render_template('relCompras.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
